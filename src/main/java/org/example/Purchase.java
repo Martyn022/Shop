983309729974887b492ab5b4074d8db1a7055cfb
Martyn022 <mgsharipov@gmail.com>
2023-05-20 11:56:36 +0500

    дописал basket, исправил purchase

--- src/main/java/org/example/Purchase.java
+++ src/main/java/org/example/Purchase.java
@@ -1,5 +1,28 @@
 package org.example;
 
 public class Purchase {
+    protected String title;
+    protected int count;
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 
 }
